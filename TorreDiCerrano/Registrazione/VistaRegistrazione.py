633264f467c77f28cb64803ea20355bccf8fdbd5
fd-col <s1093242@studenti.univpm.it>
2021-05-17 23:03:09 +0200

    1

--- TorreDiCerrano/Registrazione/VistaRegistrazione.py
+++ TorreDiCerrano/Registrazione/VistaRegistrazione.py
@@ -1,12 +1,14 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont
-
+import os
+import pickle
 
 class VistaRegistrazione(QWidget):
 
     def __init__(self, parent=None):
 
         super(VistaRegistrazione, self).__init__(parent)
+
         self.v_layout = QVBoxLayout()
         self.font_label = QFont("Arial", 17)
 
@@ -23,12 +25,14 @@ class VistaRegistrazione(QWidget):
         self.campo_nome = QLineEdit()
         self.v_layout.addWidget(self.campo_nome)
 
+
         self.label_cognome = QLabel("Cognome")
         self.label_cognome.setFont(self.font_label)
         self.v_layout.addWidget(self.label_cognome)
 
         self.campo_cognome = QLineEdit()
         self.v_layout.addWidget(self.campo_cognome)
+
 
         self.label_nascita = QLabel("Data di nascita (gg/mm/aaaa)")
         self.label_nascita.setFont(self.font_label)
@@ -37,6 +41,7 @@ class VistaRegistrazione(QWidget):
         self.campo_nascita = QLineEdit()
         self.v_layout.addWidget(self.campo_nascita)
 
+
         self.label_indirizzo = QLabel("Indirizzo")
         self.label_indirizzo.setFont(self.font_label)
         self.v_layout.addWidget(self.label_indirizzo)
@@ -44,12 +49,14 @@ class VistaRegistrazione(QWidget):
         self.campo_indirizzo = QLineEdit()
         self.v_layout.addWidget(self.campo_indirizzo)
 
+
         self.label_telefono = QLabel("Telefono")
         self.label_telefono.setFont(self.font_label)
         self.v_layout.addWidget(self.label_telefono)
 
         self.campo_telefono = QLineEdit()
         self.v_layout.addWidget(self.campo_telefono)
+
 
         self.label_email = QLabel("E-mail")
         self.label_email.setFont(self.font_label)
@@ -71,7 +78,11 @@ class VistaRegistrazione(QWidget):
         self.bottone_conferma.setFont(self.font_label)
         self.bottone_conferma.setStyleSheet("background-color:#ccd9ff;")
         self.v_layout.addWidget(self.bottone_conferma)
+        #self.bottone_conferma.clicked.connect(self.aggiungi_cliente(...))
+
 
         self.setLayout(self.v_layout)
         self.resize(200, 600)
         self.setWindowTitle("Registrazione")
+
+
