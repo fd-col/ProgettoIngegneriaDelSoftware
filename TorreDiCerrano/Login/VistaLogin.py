e56db002e1f7b63feb8e8feca5a72dc29652ce18
AndreaCiv <s1092302@studenti.univpm.it>
2021-05-18 18:07:11 +0200

    Login cliente

--- TorreDiCerrano/Login/VistaLogin.py
+++ TorreDiCerrano/Login/VistaLogin.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont
 
+from TorreDiCerrano.Cliente.controller.ControlloreCliente import ControlloreCliente
+from TorreDiCerrano.Cliente.views.VistaCliente import VistaCliente
 from TorreDiCerrano.ListaClienti.controller.ControlloreListaClienti import ControlloreListaClienti
 
 
@@ -33,6 +35,7 @@ class VistaLogin(QWidget):
         self.v_layout.addWidget(self.label_password)
 
         self.campo_password = QLineEdit()
+        self.campo_password.setEchoMode(QLineEdit.Password)
         self.v_layout.addWidget(self.campo_password)
 
         self.v_layout.addSpacing(20)
@@ -40,6 +43,7 @@ class VistaLogin(QWidget):
         self.bottone_login = QPushButton("Login")
         self.bottone_login.setFont(self.font)
         self.bottone_login.setStyleSheet("background-color:#ccd9ff;")
+        self.bottone_login.clicked.connect(self.login)
         self.v_layout.addWidget(self.bottone_login)
 
         self.setLayout(self.v_layout)
@@ -51,14 +55,17 @@ class VistaLogin(QWidget):
         password = self.campo_password.text()
 
         if self.controllore.get_cliente_by_email(email) is not None:
-            cliente = self.controllore.get_cliente_by_email(email)
+            da_mostrare = self.controllore.get_cliente_by_email(email)
+            if da_mostrare.password == password:
+                self.vista_cliente = VistaCliente(ControlloreCliente(da_mostrare))
+                self.vista_cliente.show()
+                self.close()
+            else:
+                QMessageBox.critical(self, "Errore", "La password è errata", QMessageBox.Ok,
+                                     QMessageBox.Ok)
         else:
             QMessageBox.critical(self, "Errore", "L'email inserita non è associata ad alcun cliente", QMessageBox.Ok,
                                  QMessageBox.Ok)
 
-        if cliente.password == password:
-            pass
-        else
-            QMessageBox.critical(self, "Errore", "La password è errata", QMessageBox.Ok,
-                                 QMessageBox.Ok)
+
 
