43bc868cdee8863abead65fc8d0c6ca2ed3a3c5b
AndreaCiv <s1092302@studenti.univpm.it>
2021-06-22 17:59:45 +0200

    modifica home

--- TorreDiCerrano/Home/view/TabWidget.py
+++ TorreDiCerrano/Home/view/TabWidget.py
@@ -12,14 +12,14 @@ class TabWidget(QWidget):
     def __init__(self):
         super(QWidget, self).__init__()
 
-        self.layout = QVBoxLayout(self)
+        self.layout = QVBoxLayout()
 
         # Horizontal layout for buttons and labels
         self.h_button_layout = QHBoxLayout()
 
         self.bottone_info = self.create_button("", "", "", "Info", self.visualizza_info)
         self.bottone_info.setStyleSheet("border-radius: 10px;")
-        self.bottone_info.setIcon(QIcon("images\\icon_info.png"))
+        self.bottone_info.setIcon(QIcon("images/icon_info.png"))
         self.bottone_info.setIconSize(QSize(150, 70))
 
         self.bottone_login = self.create_button("        Login        ", "rgb(255,255,255)", "rgb(0, 0, 0)",
@@ -36,9 +36,13 @@ class TabWidget(QWidget):
         # Initialize tab screen
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
+        self.tab1.setStyleSheet("background-color: #FFFFF0;")
         self.tab2 = QWidget()
+        self.tab2.setStyleSheet("background-color: #FFFFF0;")
         self.tab3 = QWidget()
+        self.tab3.setStyleSheet("background-color: #FFFFF0;")
         self.tab4 = QWidget()
+        self.tab4.setStyleSheet("background-color: #FFFFF0;")
 
         # Add tabs
         self.tabs.addTab(self.tab1, QIcon("images/icona_home.jpg"), "Home")
@@ -48,8 +52,8 @@ class TabWidget(QWidget):
         self.tabs.addTab(self.tab3, QIcon("images/icons8-lista-64.png"), "Servizi")
         self.tabs.setIconSize(QSize(30, 30))
         self.tabs.addTab(self.tab4, QIcon("images/icons8-euro-80.png"), "Prezzi")
-        self.tabs.setFont((QFont("Candara", 15)))
-        self.tabs.setStyleSheet("background-color: #FFFFF0;")  # colore sfondo tabs : avorio
+        self.tabs.setFont((QFont("Arial", 15)))
+        self.tabs.setStyleSheet("QTabBar::tab { height: 100px; width: 200px; }")
 
         # Create first tab
         self.tab1.layout = QHBoxLayout(self)
@@ -59,7 +63,7 @@ class TabWidget(QWidget):
 
         # widgets
         self.label_resort = QLabel("Resort Torre di Cerrano")
-        self.label_resort.setStyleSheet("color: rgb(51, 102, 255);\n""font: 100 30pt \"Papyrus\";\n"
+        self.label_resort.setStyleSheet("color: rgb(51, 102, 255);\n""font: 100 50pt \"Papyrus\";\n"
                                         "background-color: rgba(255, 168, 29, 0);")
         self.label_logo = QLabel()
         self.label_logo.setPixmap(QPixmap("images/icon_logo_rt.png"))
@@ -77,17 +81,17 @@ class TabWidget(QWidget):
                                         "offerti. ")
         self.label_descrizione.setWordWrap(True)
         self.label_descrizione.setMargin(15)
-        self.label_descrizione.setMinimumWidth(700)
-        self.label_descrizione.setStyleSheet("font: 14pt \"Papyrus\";")
+        self.label_descrizione.setMinimumWidth(400)
+        self.label_descrizione.setStyleSheet("font: 16pt \"Papyrus\";")
         self.label_descrizione.setAlignment(Qt.AlignCenter)
 
         # add horizontal_title_layout and label_descrizione to v_layout (vertical layout)
         self.v_layout.addLayout(self.h_title_layout)
         self.v_layout.addWidget(self.label_descrizione)
 
-        self.label_image = self.create_label_image('images\\torre_cerrano.jpg')
-        self.label_image.setMargin(25)
-        self.label_image.setFixedSize(1050, 750)
+        self.label_image = self.create_label_image('images/torre_cerrano.jpg')
+        self.label_image.setMargin(10)
+        self.label_image.setFixedSize(700, 450)
 
         # add v_layout and label_image to tab1
         self.tab1.layout.addLayout(self.v_layout)
@@ -100,18 +104,18 @@ class TabWidget(QWidget):
         self.tab2.layout = QGridLayout(self)
 
         # images to insert into tab2
-        self.label_travel = self.create_label_image("images\\travel-1677347_1920.jpg")
-        self.label_travel.setMaximumSize(550, 250)
-        self.label_sea = self.create_label_image("images\\sea-3704014_1920.jpg")
-        self.label_sea.setMaximumSize(550, 250)
-        self.label_pineta = self.create_label_image("images\\pineta.jpg")
-        self.label_pineta.setMaximumSize(550, 250)
-        self.label_piscina = self.create_label_image("images\\piscina.jpg")
-        self.label_piscina.setMaximumSize(550, 250)
-        self.label_pineta2 = self.create_label_image("images\\pineta2.jpg")
-        self.label_pineta2.setMaximumSize(550, 250)
-        self.label_ombrelloni = self.create_label_image("images\\ombrelloni.jpg")
-        self.label_ombrelloni.setMaximumSize(550, 250)
+        self.label_travel = self.create_label_image("images/travel-1677347_1920.jpg")
+        self.label_travel.setMaximumSize(400, 250)
+        self.label_sea = self.create_label_image("images/sea-3704014_1920.jpg")
+        self.label_sea.setMaximumSize(400, 250)
+        self.label_pineta = self.create_label_image("images/pineta.jpg")
+        self.label_pineta.setMaximumSize(400, 250)
+        self.label_piscina = self.create_label_image("images/piscina.jpg")
+        self.label_piscina.setMaximumSize(400, 250)
+        self.label_pineta2 = self.create_label_image("images/pineta2.jpg")
+        self.label_pineta2.setMaximumSize(400, 250)
+        self.label_ombrelloni = self.create_label_image("images/ombrelloni.jpg")
+        self.label_ombrelloni.setMaximumSize(400, 250)
 
         self.tab2.layout.addWidget(self.label_travel, 0, 0)
         self.tab2.layout.addWidget(self.label_piscina, 0, 1)
@@ -191,6 +195,7 @@ class TabWidget(QWidget):
         # Final layout
         self.layout.addLayout(self.h_button_layout)
         self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
 
     def create_button(self, testo, text_color, background_color, nome, comando):
         bottone = QPushButton(testo)
@@ -242,7 +247,7 @@ class TabWidget(QWidget):
         table_widget = QTableWidget(row, column)
 
         # table settings
-        table_widget.setFont(QFont("Candara", 18))
+        table_widget.setFont(QFont("Arial", 18))
         table_widget.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         # header
@@ -266,6 +271,6 @@ class TabWidget(QWidget):
 
     def create_check_box(self, text):
         check_box = QCheckBox(text)
-        check_box.setFont(QFont("Candara", 16))
+        check_box.setFont(QFont("Arial", 16))
         check_box.setChecked(True)
         return check_box
